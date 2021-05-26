4d2bd0b0f9826d89d63438a347e9da2d88f05fb5
aaaryya1 <79969052+aaaryya1@users.noreply.github.com>
2021-05-26 13:40:56 +0530

    program 8

---
+++ First 15 programs./Average.java
@@ -0,0 +1,11 @@
+public class Average {
+
+   public static void main(String[] args) {
+ 
+int n1=32,n2=1,n3=12,n4=11,n5=2;
+float sum=n1+n2+n3+n4+n5;
+float avg= sum/5;
+System.out.println("The sum of 5 numbers"+sum);
+System.out.println("The average of 5 numbers"+avg);
+}
+}
