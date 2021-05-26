439174252ac8630869eefdf2562b823f1d0065b2
aaaryya1 <79969052+aaaryya1@users.noreply.github.com>
2021-05-26 13:46:20 +0530

    program 12

---
+++ First 15 programs./Forloop.java
@@ -0,0 +1,16 @@
+public class Forloop {
+  
+ 
+    public static void main(String[] args)
+    {
+   
+        int[] arr = {9, 2, 5, 76,11};
+  
+        System.out.print("Elements of given array are: ");
+  
+        for (int i = 0; i < arr.length; i++) 
+        {      
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
