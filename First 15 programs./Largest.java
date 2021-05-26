ff7f1d0cc3012493b8155dd79d4937788dd93121
aaaryya1 <79969052+aaaryya1@users.noreply.github.com>
2021-05-26 14:07:47 +0530

    program 6

---
+++ First 15 programs./Largest.java
@@ -0,0 +1,19 @@
+public class Largest {
+
+    public static void main(String[] args) {
+
+        int n1 = 12, n2 = 987, n3 = 93;
+
+        if(n1 >= n2) {
+            if(n1 >= n3)
+                System.out.println(n1 + " is the largest number.");
+            else
+                System.out.println(n3 + " is the largest number.");
+        } else {
+            if(n2 >= n3)
+                System.out.println(n2 + " is the largest number.");
+            else
+                System.out.println(n3 + " is the largest number.");
+        }
+    }
+}
