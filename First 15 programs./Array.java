b88d8c08eff3fab349193109599036fa0e310aff
aaaryya1 <79969052+aaaryya1@users.noreply.github.com>
2021-05-26 13:57:21 +0530

    program 11

---
+++ First 15 programs./Array.java
@@ -0,0 +1,14 @@
+public class Array {
+ public static void main(String[] args) {
+  
+    
+   int[] arr = {0, 1, 2, 3};
+ 
+   System.out.println("Accessing Elements of Array:");
+   System.out.println("First Element: " + arr[0]);
+   System.out.println("Second Element: " + arr[1]);
+   System.out.println("Third Element: " + arr[2]);
+   System.out.println("Fourth Element: " + arr[3]);
+   
+ }
+}
